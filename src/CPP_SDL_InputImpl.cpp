ad8b31b46b28477601eddfe30a1db131f7b786a9
Wade Okawa-Scannell <okawaw@gmail.com>
2017-04-30 19:17:27 -0400

    Add support for more keys.

--- src/CPP_SDL_InputImpl.cpp
+++ src/CPP_SDL_InputImpl.cpp
@@ -10,6 +10,7 @@
 
 #include "CPP_Key.h"
 
+#include <iostream>
 #include <utility>
 
 // TODO: Fix includes.
@@ -48,7 +49,56 @@ CPP_Key translateKey(const SDL_Keycode key)
 			ret = CPP_Key::ENTER;
 			break;
 		}
-		
+		case SDLK_LGUI:
+		case SDLK_RGUI:
+		{
+			ret = CPP_Key::COMMAND;
+			break;
+		}
+		case SDLK_LCTRL:
+		case SDLK_RCTRL:
+		{
+			ret = CPP_Key::CONTROL;
+			break;
+		}
+		case SDLK_SPACE:
+		// TODO: case SDLK_KP_SPACE:?
+		{
+			ret = CPP_Key::SPACE;
+			break;
+		}
+		case SDLK_LSHIFT:
+		case SDLK_RSHIFT:
+		{
+			ret = CPP_Key::SHIFT;
+			break;
+		}
+		case SDLK_BACKSPACE:
+		// TODO: case SDLK_KP_BACKSPACE:?
+		{
+			ret = CPP_Key::BACKSPACE;
+			break;
+		}
+		case SDLK_CAPSLOCK:
+		{
+			ret = CPP_Key::CAPS_LOCK;
+			break;
+		}
+		case SDLK_DELETE:
+		{
+			ret = CPP_Key::DELETE;
+			break;
+		}
+		case SDLK_END:
+		{
+			ret = CPP_Key::END;
+			break;
+		}
+		case SDLK_ESCAPE:
+		{
+			ret = CPP_Key::ESCAPE;
+			break;
+		}
 		// TODO: SDLK_AC_HOME?
 		case SDLK_HOME:
 		{
@@ -60,20 +110,20 @@ CPP_Key translateKey(const SDL_Keycode key)
 			ret = CPP_Key::INSERT;
 			break;
 		}
-		// TODO: SDLK_KP_TAB?
 		case SDLK_TAB:
+		// TODO: case SDLK_KP_TAB:?
 		{
 			ret = CPP_Key::TAB;
-			break;
-		}
-		case SDLK_PAGEUP:
-		{
-			ret = CPP_Key::PAGE_UP;
 			break;
 		}
 		case SDLK_PAGEDOWN:
 		{
 			ret = CPP_Key::PAGE_DOWN;
+			break;
+		}
+		case SDLK_PAGEUP:
+		{
+			ret = CPP_Key::PAGE_UP;
 			break;
 		}
 		case SDLK_LEFTBRACKET:
@@ -87,56 +137,6 @@ CPP_Key translateKey(const SDL_Keycode key)
 			break;
 		}
 		
-		case SDLK_0:
-		{
-			ret = CPP_Key::DIGIT_0;
-			break;
-		}
-		case SDLK_1:
-		{
-			ret = CPP_Key::DIGIT_1;
-			break;
-		}
-		case SDLK_2:
-		{
-			ret = CPP_Key::DIGIT_2;
-			break;
-		}
-		case SDLK_3:
-		{
-			ret = CPP_Key::DIGIT_3;
-			break;
-		}
-		case SDLK_4:
-		{
-			ret = CPP_Key::DIGIT_4;
-			break;
-		}
-		case SDLK_5:
-		{
-			ret = CPP_Key::DIGIT_5;
-			break;
-		}
-		case SDLK_6:
-		{
-			ret = CPP_Key::DIGIT_6;
-			break;
-		}
-		case SDLK_7:
-		{
-			ret = CPP_Key::DIGIT_7;
-			break;
-		}
-		case SDLK_8:
-		{
-			ret = CPP_Key::DIGIT_8;
-			break;
-		}
-		case SDLK_9:
-		{
-			ret = CPP_Key::DIGIT_9;
-			break;
-		}
 		case SDLK_a:
 		{
 			ret = CPP_Key::A;
@@ -267,8 +267,218 @@ CPP_Key translateKey(const SDL_Keycode key)
 			ret = CPP_Key::Z;
 			break;
 		}
+		
+		case SDLK_F1:
+		{
+			ret = CPP_Key::F1;
+			break;
+		}
+		case SDLK_F2:
+		{
+			ret = CPP_Key::F2;
+			break;
+		}
+		case SDLK_F3:
+		{
+			ret = CPP_Key::F3;
+			break;
+		}
+		case SDLK_F4:
+		{
+			ret = CPP_Key::F4;
+			break;
+		}
+		case SDLK_F5:
+		{
+			ret = CPP_Key::F5;
+			break;
+		}
+		case SDLK_F6:
+		{
+			ret = CPP_Key::F6;
+			break;
+		}
+		case SDLK_F7:
+		{
+			ret = CPP_Key::F7;
+			break;
+		}
+		case SDLK_F8:
+		{
+			ret = CPP_Key::F8;
+			break;
+		}
+		case SDLK_F9:
+		{
+			ret = CPP_Key::F9;
+			break;
+		}
+		case SDLK_F10:
+		{
+			ret = CPP_Key::F10;
+			break;
+		}
+		case SDLK_F11:
+		{
+			ret = CPP_Key::F11;
+			break;
+		}
+		case SDLK_F12:
+		{
+			ret = CPP_Key::F12;
+			break;
+		}
+		case SDLK_F13:
+		{
+			ret = CPP_Key::F13;
+			break;
+		}
+		case SDLK_F14:
+		{
+			ret = CPP_Key::F14;
+			break;
+		}
+		case SDLK_F15:
+		{
+			ret = CPP_Key::F15;
+			break;
+		}
+		
+		case SDLK_0:
+		{
+			ret = CPP_Key::DIGIT_0;
+			break;
+		}
+		case SDLK_1:
+		{
+			ret = CPP_Key::DIGIT_1;
+			break;
+		}
+		case SDLK_2:
+		{
+			ret = CPP_Key::DIGIT_2;
+			break;
+		}
+		case SDLK_3:
+		{
+			ret = CPP_Key::DIGIT_3;
+			break;
+		}
+		case SDLK_4:
+		{
+			ret = CPP_Key::DIGIT_4;
+			break;
+		}
+		case SDLK_5:
+		{
+			ret = CPP_Key::DIGIT_5;
+			break;
+		}
+		case SDLK_6:
+		{
+			ret = CPP_Key::DIGIT_6;
+			break;
+		}
+		case SDLK_7:
+		{
+			ret = CPP_Key::DIGIT_7;
+			break;
+		}
+		case SDLK_8:
+		{
+			ret = CPP_Key::DIGIT_8;
+			break;
+		}
+		case SDLK_9:
+		{
+			ret = CPP_Key::DIGIT_9;
+			break;
+		}
+		
+		case SDLK_KP_0:
+		{
+			ret = CPP_Key::NUMPAD_0;
+			break;
+		}
+		case SDLK_KP_1:
+		{
+			ret = CPP_Key::NUMPAD_1;
+			break;
+		}
+		case SDLK_KP_2:
+		{
+			ret = CPP_Key::NUMPAD_2;
+			break;
+		}
+		case SDLK_KP_3:
+		{
+			ret = CPP_Key::NUMPAD_3;
+			break;
+		}
+		case SDLK_KP_4:
+		{
+			ret = CPP_Key::NUMPAD_4;
+			break;
+		}
+		case SDLK_KP_5:
+		{
+			ret = CPP_Key::NUMPAD_5;
+			break;
+		}
+		case SDLK_KP_6:
+		{
+			ret = CPP_Key::NUMPAD_6;
+			break;
+		}
+		case SDLK_KP_7:
+		{
+			ret = CPP_Key::NUMPAD_7;
+			break;
+		}
+		case SDLK_KP_8:
+		{
+			ret = CPP_Key::NUMPAD_8;
+			break;
+		}
+		case SDLK_KP_9:
+		{
+			ret = CPP_Key::NUMPAD_9;
+			break;
+		}
+		case SDLK_KP_PLUS:
+		{
+			ret = CPP_Key::NUMPAD_ADD;
+			break;
+		}
+		case SDLK_KP_DECIMAL:
+		{
+			ret = CPP_Key::NUMPAD_DECIMAL;
+			break;
+		}
+		case SDLK_KP_DIVIDE:
+		{
+			ret = CPP_Key::NUMPAD_DIVIDE;
+			break;
+		}
+		case SDLK_KP_ENTER:
+		{
+			ret = CPP_Key::NUMPAD_ENTER;
+			break;
+		}
+		case SDLK_KP_MULTIPLY:
+		{
+			ret = CPP_Key::NUMPAD_MULTIPLY;
+			break;
+		}
+		case SDLK_KP_MINUS:
+		{
+			ret = CPP_Key::NUMPAD_SUBTRACT;
+			break;
+		}
+		
 		default:
 		{
+			std::cout << "Key is unsupported." << std::endl;
 			ret = CPP_Key::ANY;
 			break;
 		}
